1f5d58e348f291ace7dde3da163724acd59117f3
Tri Le <letrin93@gmail.com>
2022-11-21 11:11:40 -0700

    spells tl

--- dndtracker.client/src/pages/AboutPage.vue
+++ dndtracker.client/src/pages/AboutPage.vue
@@ -1,15 +1,33 @@
 <template>
-  <div class="about">
-    <SpellCard/>
-  </div>
+  <!-- <div class="about" v-for="s in spells" :key="s.id">
+    <SpellCard :spell="s"/>
+  </div> -->
 </template>
 
 <script>
+import { computed } from "@vue/reactivity";
+import { onMounted } from "vue";
+import { AppState } from "../AppState.js";
 import SpellCard from "../components/SpellCard.vue";
+import { spellsService } from "../services/SpellsService.js";
+import Pop from "../utils/Pop.js";
 
 export default {
-    setup() {
-        return {};
+  setup() {
+    onMounted(() => {
+      getSpells()
+    });
+    async function getSpells() {
+      try {
+          await spellsService.getSpells()
+        } catch (error) {
+          console.error('[getSpells]',error)
+          Pop.error(error)
+        }
+    }
+    return {
+          // spells: computed(() => AppState.spells),
+        };
     },
     components: { SpellCard }
 }
