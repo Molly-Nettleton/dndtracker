bae22088bb907f19a468d7076195dd44fa9800f0
Tri Le <letrin93@gmail.com>
2022-11-21 10:12:59 -0700

    tru

---
+++ dndtracker.client/src/components/SpellCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="component col-md-3">
+
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  setup(){
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
