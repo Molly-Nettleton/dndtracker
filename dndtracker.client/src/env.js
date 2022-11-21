937d91964cbce8b65ba3db0e16213188a0de0e37
daniel le <daniel97.le@gmail.com>
2022-11-21 09:41:09 -0700

    commit

--- dndtracker.client/src/env.js
+++ dndtracker.client/src/env.js
@@ -1,6 +1,6 @@
-export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'https://localhost:7045' : ''
-export const useSockets = false
-export const domain = 'mn97.us.auth0.com'
-export const audience = 'https://codeworks-demo.com'
-export const clientId = '65XsU6GU9gesQJLZrI98Tfms0x3jcUIw'
+export const dev = window.location.origin.includes("localhost");
+export const baseURL = dev ? "https://localhost:7045" : "";
+export const useSockets = false;
+export const domain = "bobocat.us.auth0.com";
+export const audience = "https://bcw-demos.com";
+export const clientId = "Acd91zZtHgbEyChg4MW53440xBdm6cqE";
