ab6eaf793cf00f6b24ce6a8c4be919f4474306b0
TungLe0319 <tung.le0319@gmail.com>
2022-11-21 13:10:39 -0700

    tung

--- dndtracker.client/src/pages/HomePage.vue
+++ dndtracker.client/src/pages/HomePage.vue
@@ -2,54 +2,7 @@
   <section class="container">
     <div class="d-flex justify-content-center p-3">
 
-      <div class="card charcard bg-dark text-white border border-0 elevation-5 hoverable hover ms-4">
-        <img src="src\assets\img\character-images\PRLGmBW.png" class="card-img rounded-0 rounded-top" alt="..."
-          height="250" width="250">
-        <div class="card-footer d-flex justify-content-between bg-dark">
-          <div class="card-title fontbg fs-3 fw-bold text-lowercase">
-            <span class="title">Imogen</span>
-          </div>
-          <div class="d-flex align-items-center">
-            <img src="src\assets\img\class-icons\Class Icon - Warlock.svg" height="40" width="40"
-              class="rounded-circle elevation-2 border border-2 bg-white ms-1" alt="">
-            <img src="src\assets\img\class-icons\Class Icon - Rogue.svg" height="40" width="40"
-              class="rounded-circle elevation-2 border border-2 bg-white ms-1" alt="">
-          </div>
-        </div>
-      </div>
-
-
-      <div class="card charcard bg-dark text-white border border-0 elevation-5 hoverable hover ms-4">
-        <img src="src\assets\img\character-images\ren.png" class="card-img rounded-0 rounded-top" alt="..." height="250"
-          width="250">
-        <div class="card-footer d-flex justify-content-between bg-dark">
-          <div class="card-title fontbg fs-3 fw-bold text-lowercase">
-            <span class="title">Ren</span>
-          </div>
-          <div class="d-flex align-items-center">
-            <img src="src\assets\img\class-icons\Class Icon - Paladin.svg" height="40" width="40"
-              class="rounded-circle elevation-2 border border-2 bg-white ms-1" alt="">
-            <img src="src\assets\img\class-icons\Class Icon - Warlock.svg" height="40" width="40"
-              class="rounded-circle elevation-2 border border-2 bg-white ms-1" alt="">
-          </div>
-        </div>
-      </div>
-
-      <div class="card charcard bg-dark text-white border border-0 elevation-5 hoverable hover ms-4">
-        <img src="src\assets\img\character-images\28bPgSa.png" class="card-img rounded-0 rounded-top" alt="..."
-          height="250" width="250">
-        <div class="card-footer d-flex justify-content-between bg-dark">
-          <div class="card-title fontbg fs-3 fw-bold text-lowercase">
-            <span class="title">Calith</span>
-          </div>
-          <div class="d-flex align-items-center">
-            <img src="src\assets\img\class-icons\Class Icon - Wizard.svg" height="40" width="40"
-              class="rounded-circle elevation-2 border border-2 bg-white ms-1" alt="">
-          </div>
-        </div>
-      </div>
-
-
+     <CharacterCard/>
 
     </div>
   </section>
@@ -58,22 +11,24 @@
 
 <script>
 import { onMounted } from "vue";
+import CharacterCard from "../components/CharacterCard.vue";
 import { spellsService } from "../services/SpellsService.js";
 
 export default {
-  setup() {
-    onMounted(() => {
-      // getSpells()
-    });
-    // async function getSpells() {
-    //   try {
-    //     await spellsService.getSpells();
-    //   } catch (error) {
-    //     Pop.error(error, "[getSpells]");
-    //   }
-    // }
-    return {};
-  },
+    setup() {
+        onMounted(() => {
+            // getSpells()
+        });
+        // async function getSpells() {
+        //   try {
+        //     await spellsService.getSpells();
+        //   } catch (error) {
+        //     Pop.error(error, "[getSpells]");
+        //   }
+        // }
+        return {};
+    },
+    components: { CharacterCard }
 };
 </script>
 
