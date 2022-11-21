6794d88373fd2d7d75d3c5ab5415162db9bb2b32
molly-nettleton <mollyelainen@gmail.com>
2022-11-21 09:16:17 -0700

    ENV

--- dndtracker.client/src/env.js
+++ dndtracker.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'https://localhost:7045/' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'mn97.us.auth0.com'
+export const audience = 'https://codeworks-demo.com'
+export const clientId = '65XsU6GU9gesQJLZrI98Tfms0x3jcUIw'
