35f9d2af108803cef7da4d7476ef664abd4d6cea
Tri Le <letrin93@gmail.com>
2022-11-21 11:04:03 -0700

    tri

--- dndtracker.client/src/components/SpellCard.vue
+++ dndtracker.client/src/components/SpellCard.vue
@@ -2,46 +2,42 @@
   <section class="container">
     <div class="row">
       <div class="col-md-3 bg-danger">
-        <h3 class="text-center text-dark bg-white mt-3 p-1 mb-0 rounded-top">ThunderClap</h3>
-        <p class="bg-danger text-white text-center mb-0">Evocation cantrip</p>
+        <h3 class="text-center text-dark bg-white mt-3 p-1 mb-0 rounded-top">{{spell?.name}}</h3>
+        <p class="bg-danger text-white text-center mb-0">{{spell?.school}}  {{spell?.level}}</p>
         <div class="row">
           <div class="col-6 text-center ">
             <div class="bg-white">
               <h5 class="text-danger">Casting Time</h5>
-              <p class="text-dark">1 action</p>
+              <p class="text-dark">{{spell?.casting_time}}</p>
             </div>
           </div>
           <div class="col-6 text-center">
             <div class="bg-white">
               <h5 class="text-danger">Range</h5>
-              <p class="text-dark">Self (5 foot radius)</p>
+              <p class="text-dark">{{spell?.range}}</p>
             </div>
           </div>
           <div class="col-6 text-center">
             <div class="bg-white">
               <h5 class="text-danger">Components</h5>
-              <p class="text-dark">S</p>
+              <p class="text-dark">{{spell?.components}}</p>
             </div>
           </div>
           <div class="col-6 text-center">
             <div class="bg-white">
               <h5 class="text-danger">Duration</h5>
-              <p class="text-dark">Instantaneous</p>
+              <p class="text-dark">{{spell?.duration}}</p>
             </div>
           </div>
         </div>
         <div class="row">
           <div class="col-12">
-            <p class="bg-white text-dark p-2 mb-0">You create a burst of thunderous sound that can be heard up to 100 feet
-              away. Each creature within
-              range, other than
-              you, must succeed on a Constitution saving throw or take 1d6 thunder damage.</p>
+            <p class="bg-white text-dark p-2 mb-0">{{spell?.desc}}</p>
             <div class="col-12 text-center text-white">
               <p class="mb-0">At Higher Levels</p>
             </div>
             <div class="col-12 bg-white text-dark mb-3 rounded-bottom">
-              <p class="p-2">The spell's damage increases by 1d6 when
-                you reach 5th level (2d6), 11th level (3d6), and 17th level (4d6)</p>
+              <p class="p-2">{{spell?.higher_level}}</p>
             </div>
           </div>
         </div>
