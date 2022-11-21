cd10a269dcb7b88a31761e18fdd717204587b46d
Tri Le <letrin93@gmail.com>
2022-11-21 13:52:19 -0700

    tri

---
+++ dndtracker/Models/Spell.cs
@@ -0,0 +1,16 @@
+namespace dndtracker.Models;
+
+public class Spell : IDbItem<int>
+{
+public string Name { get; set; }
+public string Descriptions { get; set; }
+public string HigherLevel { get; set; }
+public string Range { get; set; }
+public string Components { get; set; }
+public string Material { get; set; }
+public string AOE { get; set; }
+
+  public int Id { get ; set ; }
+  public DateTime CreatedAt { get ; set ; }
+  public DateTime UpdatedAt { get ; set ; }
+}
