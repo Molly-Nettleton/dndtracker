58d5d621482059850476ed9c3c34862243b7b082
daniel le <daniel97.le@gmail.com>
2022-11-21 17:18:43 -0700

    commit

--- dndtracker/Models/Character.cs
+++ dndtracker/Models/Character.cs
@@ -2,20 +2,22 @@ namespace dndtracker.Models;
 public class Character : IHasCreator
 {
 
-  public int Id { get; set; }
+
   public string Name { get; set; }
   public int Level { get; set; }
   public int Hp { get; set; }
   public string Img { get; set; }
-  public List<Classes> Classes { get; set; }
-  public List<Classes> SecondClasses { get; set; }
+  public string Classes { get; set; }
+  public string SecondClasses { get; set; }
   public string SubClass { get; set; }
   public string Race { get; set; }
   // public AbilityScores AbilityScores { get; set; }
   public string Background { get; set; }
-  // Populated
   public string CreatorId { get; set; }
   public Profile Creator { get; set; }
+  public int Id { get; set; }
   public DateTime CreatedAt { get; set; }
   public DateTime UpdatedAt { get; set; }
+  // Populated
+
 }
